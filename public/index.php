280d885d35410a609005f28dc21a30d115686c5b
DanielAlk <daniodfad@yahoo.com.ar>
2015-09-02 09:14:38 -0300

    Get rid completely of cache_control method, and $page_headers global variavble

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
 include '../environment/init.php';
-if (isset($page_headers)) foreach ($page_headers as $header) header($header);
 include $app->layout;
 if (method_exists($app, 'buffer')) $app->buffer();
 ?>
