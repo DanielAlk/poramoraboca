1399228c76f7ef1ec6ced472a86c465b23a43768
DanielAlk <daniodfad@yahoo.com.ar>
2015-11-04 20:44:16 -0300

    responsive home changes

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -8,19 +8,21 @@ extra_js;
 	<div class="relativize expand" id="home_slider">
 		<div class="slide bg-bombonera360 expand active" id="slide_1">
 			<div class="container">
-				<div class="slide-caption">
+				<div class="slide-caption top-p-75-xs">
 					<div><img class="cabj-and-logo" src="<?php $asset->path('cabj-bombonera360.png') ?>"></div>
 					<h2 class="text-gold italic"><big class="hashtag">#DeLaBomboneraNoNosVamos</big></h2>
 					<p class="text-white">
-						Bombonera360 es nuestra propuesta para la ampliación y reforma integral del Estadio Alberto J. Armando sin ceder la localía ni endeudar al Club.<br>
-						<br>
+						Bombonera360 es nuestra propuesta para la ampliación y reforma integral del Estadio Alberto J. Armando sin ceder la localía ni endeudar al Club.
+						<br><br class="hidden-xs">
 						<big class="text-gold italic bold">Se puede.</big>
 					</p>
 					<a class="btn btn-primary" target="_blank" href="http://bombonera360.com.ar/">VER PROYECTO</a>
 				</div>
 			</div>
-			<div class="absolute bottom slide-image">
-				<img src="<?php $asset->path('jorge-amor-ameal.png') ?>">
+			<div class="absolute bottom slide-image slide-image-1">
+				<div>
+					<img src="<?php $asset->path('ameal-pergolini.png') ?>">
+				</div>
 			</div>
 		</div>
 		<div class="slide bg-banderas expand" id="slide_2">
@@ -28,14 +30,14 @@ extra_js;
 				<div class="slide-caption">
 					<div class="space-S-50 space-S-0-xs">
 						<img class="pull-left cancel-pull-xs space-E-15" src="<?php $asset->path('icon-heart-big.png') ?>">
-						<span class="h3 uppercase italic hidden-xs"><big>El "Patrón" Bermudez se sumó al proyecto de Jorge Amor Ameal</big></span>
-						<div class="h3 uppercase italic visible-xs">El "Patrón" Bermudez se sumó al proyecto de Jorge Amor Ameal</div>
+						<span class="h3 uppercase italic hidden-xs"><big>El Patrón Bermudez, director deportivo.</big></span>
+						<div class="h3 uppercase italic visible-xs">El Patrón Bermudez, director deportivo.</div>
 					</div>
-					<p class="text-white">El histórico caudillo de Boca Juniors se unió a Juntos por Boca para hacerse cargo de la dirección deportiva del Xeneize. Junto a Oscar Regenhardt y Aníbal Matellán, llevará adelante un proyecto integral que abarca el fútbol amateur, las divisiones inferiores y el fútbol profesional.</p>
+					<p class="text-white">Junto a Regenhardt y Aníbal Matellán, presentaron un proyecto integral que abarca el fútbol amateur, las divisiones inferiores y el plantel profesional.</p>
 					<a class="btn btn-primary" href="#" data-toggle="modal" data-target="#modal_patron">CONOCÉ MÁS</a>
 				</div>
 			</div>
-			<div class="absolute bottom slide-image">
+			<div class="absolute bottom slide-image slide-image-space-left">
 				<img src="<?php $asset->path('patron-bermudez.png') ?>">
 			</div>
 		</div>
